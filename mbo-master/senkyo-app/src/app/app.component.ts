74b09470890c9ea756ab9d0c945e82085540bd29
koga_ak <koga.ak@jnovelty.co.jp>
2019-06-13 19:26:26 +0900

    第三章　Angularの動く仕組み実装
    index.html 見出しを変更
    app.component.html 被選挙人名簿追加
    app.component.ts 被選挙人列挙

--- mbo-master/senkyo-app/src/app/app.component.ts
+++ mbo-master/senkyo-app/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'senkyo-app';
+  title = '2019年役員選挙';
+  members=[
+    { id: 1, name: "バッハ" },
+    { id: 2, name: "ヘンデル" },
+    { id: 3, name: "ビバルディ" },
+    { id: 4, name: "メンデルスゾーン" },
+    { id: 5, name: "ハイドン" },
+    { id: 6, name: "ヴェルディ" },
+    { id: 7, name: "ロッシーニ" },
+    { id: 8, name: "モーツァルト" },
+    { id: 9, name: "チャイコフスキー" },
+  ];
 }
